4dfa614edc9ad823f1b0ee0b35592cd7d695c8e5
ds-muzalevskiy <74115150+ds-muzalevskiy@users.noreply.github.com>
2021-04-26 19:04:54 +0300

    hyperopt_class

---
+++ hyperopt_class.py
@@ -0,0 +1,42 @@
+import lightgbm as lgb
+import xgboost as xgb
+import catboost as ctb
+from hyperopt import fmin, tpe, STATUS_OK, STATUS_FAIL, Trials
+
+
+class HPOpt(object):
+
+    def __init__(self, x_train, x_test, y_train, y_test):
+        self.x_train = x_train
+        self.x_test  = x_test
+        self.y_train = y_train
+        self.y_test  = y_test
+
+    def process(self, fn_name, space, trials, algo, max_evals):
+        fn = getattr(self, fn_name)
+        try:
+            result = fmin(fn=fn, space=space, algo=algo, max_evals=max_evals, trials=trials)
+        except Exception as e:
+            return {'status': STATUS_FAIL,
+                    'exception': str(e)}
+        return result, trials
+
+    def xgb_reg(self, para):
+        reg = xgb.XGBRegressor(**para['reg_params'])
+        return self.train_reg(reg, para)
+
+    def lgb_reg(self, para):
+        reg = lgb.LGBMRegressor(**para['reg_params'])
+        return self.train_reg(reg, para)
+
+    def ctb_reg(self, para):
+        reg = ctb.CatBoostRegressor(**para['reg_params'])
+        return self.train_reg(reg, para)
+
+    def train_reg(self, reg, para):
+        reg.fit(self.x_train, self.y_train,
+                eval_set=[(self.x_train, self.y_train), (self.x_test, self.y_test)],
+                **para['fit_params'])
+        pred = reg.predict(self.x_test)
+        loss = para['loss_func'](self.y_test, pred)
+        return {'loss': loss, 'status': STATUS_OK}
